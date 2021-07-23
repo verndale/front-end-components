e611e3ea6ffed02d997e915409824d7856d839d5
Joe Fusco <joe.fusco@verndale.com>
2021-07-22 21:47:32 -0400

    Add JS for accordion and move lib files

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -1,4 +1,6 @@
 import { Component } from '@verndale/core';
+import { v4 as uuidv4 } from 'uuid';
+import { keyCode, open, close } from '../helpers';
 
 /**
  * `Accordion`
@@ -8,22 +10,134 @@ import { Component } from '@verndale/core';
  * import { Accordion } from '@verndale/front-end-components';
  *
  * class Foo {
- *   construction(el){
- *     super(el);
+ *   construction(){
+ *
+ *     cont Accordion = new Accordion('.accordion');
  *   }
  * }
  */
 class Accordion extends Component {
   constructor(el) {
     super(el);
+
+    this.initAccordion();
   }
 
-  setupDefaults(){
-    console.log('defaults');
+  setupDefaults() {
+    this.dom = {
+      container: this.el.querySelector('.accordion__items'),
+      items: this.el.querySelectorAll('.accordion__item'),
+      triggers: [...this.el.querySelectorAll('.accordion__item-trigger')]
+    };
   }
 
-  addListeners(){
-    console.log('listeners');
+  addListeners() {
+    this.dom.container.addEventListener(
+      'keydown',
+      this.handleKeyDown.bind(this)
+    );
+    this.dom.triggers.forEach(trigger =>
+      trigger.addEventListener('click', this.handleClick.bind(this))
+    );
+  }
+
+  initAccordion() {
+    this.dom.items.forEach((item, index) => {
+      const guid = uuidv4();
+
+      const trigger = item.querySelector('.accordion__item-trigger');
+      const content = item.querySelector('.accordion__item-panel');
+
+      if (trigger.getAttribute('aria-expanded') === 'true') {
+        content.style.height = 'auto';
+        content.style.visibility = 'visible';
+      }
+
+      trigger.setAttribute('aria-controls', `tabcontent-${guid}`);
+      trigger.setAttribute('id', `tab-${guid}`);
+      content.setAttribute('aria-labelledby', `tab-${guid}`);
+      content.setAttribute('id', `tabcontent-${guid}`);
+
+      content.setAttribute(
+        'aria-hidden',
+        trigger.getAttribute('aria-expanded') !== 'true'
+      );
+    });
+  }
+
+  handleClick(e) {
+    const button = e.currentTarget;
+
+    if (button.getAttribute('aria-expanded') === 'true') {
+      this.closeItem(button);
+    } else {
+      this.openItem(button);
+    }
+  }
+
+  handleKeyDown(e) {
+    const target = e.target;
+    const key = e.which || e.keyCode;
+
+    const ctrlModifier =
+      e.ctrlKey && (key === keyCode.PAGEUP || key === keyCode.PAGEDOWN);
+
+    if (target.classList.contains('accordion__item-trigger')) {
+      if (key === keyCode.UP || key === keyCode.DOWN || ctrlModifier) {
+        const index = this.dom.triggers.indexOf(target);
+        const direction =
+          key === keyCode.PAGEDOWN || key === keyCode.DOWN ? 1 : -1;
+        const length = this.dom.triggers.length;
+        const newIndex = (index + length + direction) % length;
+
+        this.dom.triggers[newIndex].focus();
+
+        e.preventDefault();
+      } else if (key === keyCode.END || key === keyCode.HOME) {
+        switch (key) {
+        case keyCode.HOME:
+          this.dom.triggers[0].focus();
+          break;
+        case keyCode.END:
+          this.dom.triggers[this.dom.triggers.length - 1].focus();
+          break;
+        }
+        e.preventDefault();
+      }
+    }
+  }
+
+  openItem(trigger) {
+    const content = document.getElementById(
+      trigger.getAttribute('aria-controls')
+    );
+
+    trigger.setAttribute('aria-expanded', true);
+    content.style.visibility = 'visible';
+    open({
+      element: content,
+      ease: 'easeInOut',
+      onComplete: () => {
+        content.setAttribute('aria-hidden', false);
+        content.style.height = 'auto';
+      }
+    });
+  }
+
+  closeItem(trigger) {
+    const content = document.getElementById(
+      trigger.getAttribute('aria-controls')
+    );
+
+    trigger.setAttribute('aria-expanded', false);
+    close({
+      element: content,
+      ease: 'easeInOut',
+      onComplete: () => {
+        content.setAttribute('aria-hidden', true);
+        content.style.visibility = 'hidden';
+      }
+    });
   }
 }
 
---
+++ src/components/helpers/index.js
@@ -0,0 +1,97 @@
+import { gsap, Expo, CSSPlugin } from 'gsap';
+
+gsap.registerPlugin(CSSPlugin);
+
+/**
+ * Opens an element
+ *
+ * @example
+ * import {open} from '../helpers';
+ *
+ * open({ element: this.el.querySelector('.element)});
+ *
+ * @param {Object} element - HTML element you want to target for animation
+ * @param {Number} duration - Speed of the animation
+ * @param {String} ease - Ease of the animation
+ * @param {Function} onComplete - On complete function
+ */
+export const open = ({
+  element,
+  duration = 0.4,
+  ease = 'easeOut',
+  onComplete
+}) => {
+  gsap.set(element, { height: 'auto' });
+  gsap.from(element, {
+    height: 0,
+    ease: Expo[ease],
+    duration,
+    onComplete: () => {
+      element.style.height = null;
+      onComplete && onComplete();
+    }
+  });
+};
+
+/**
+ * Closes an element
+ *
+ * @example
+ * import {close} from '../helpers';
+ *
+ * close({ element: this.el.querySelector('.element)});
+ *
+ * @param {Object} element - HTML element you want to target for animation
+ * @param {Number} duration - Speed of the animation
+ * @param {String} ease - Ease of the animation
+ * @param {Function} onComplete - On complete function
+ */
+export const close = ({
+  element,
+  duration = 0.4,
+  ease = 'easeOut',
+  onComplete
+}) => {
+  gsap.set(element, { height: element.offsetHeight });
+  gsap.to(element, {
+    height: 0,
+    ease: Expo[ease],
+    duration,
+    onComplete: () => {
+      element.style.height = null;
+      onComplete && onComplete();
+    }
+  });
+};
+
+/**
+ * Debounce function
+ * Delays the processing of the event
+ */
+export const debounce = (callback, wait) => {
+  let timerId;
+  return (...args) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, wait);
+  };
+};
+
+/**
+ * Key code list object
+ */
+export const keyCode = {
+  TAB: 9,
+  RETURN: 13,
+  ESC: 27,
+  SPACE: 32,
+  PAGEUP: 33,
+  PAGEDOWN: 34,
+  END: 35,
+  HOME: 36,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
