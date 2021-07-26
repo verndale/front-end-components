50d1db698b5bebec0127138df6adb633ca328f8b
Esteban <esteban_fs@hotmail.com>
2021-07-26 18:01:40 -0500

    Added Alert JS Functionality

--- src/components/preview.js
+++ src/components/preview.js
@@ -1,12 +1,16 @@
-import create from '@verndale/core';
+import create from '@verndale/core'
 
 const components = [
   {
     name: 'accordion',
     loader: () => import('./accordion/Accordion')
+  },
+  {
+    name: 'alert',
+    loader: () => import('./alert/Alert')
   }
-];
+]
 
 document.addEventListener('DOMContentLoaded', () => {
-  create(components);
-});
+  create(components)
+})
