16152821eb47019dec84a7d1826f2c315f0a101e
Byron Dominguez <byron.dominguez@verndale.com>
2021-07-30 13:24:42 -0500

    feat: Add variants

--- src/components/link-list/link-list.config.js
+++ src/components/link-list/link-list.config.js
@@ -5,16 +5,99 @@ module.exports = {
     links: [
       {
         label: "Explore experimental learning",
-        icon: "arrow-right",
+        icon: "chevron-right",
+        iconPosition: "right",
+        size: "small",
       },
       {
         label: "Explore experimental learning",
-        icon: "arrow-right",
+        icon: "chevron-right",
+        iconPosition: "right",
+        size: "small",
       },
       {
         label: "Explore experimental learning",
-        icon: "arrow-right",
+        icon: "chevron-right",
+        iconPosition: "right",
+        size: "small",
       },
     ],
   },
+  variants: [
+    {
+      name: "Small",
+      context: {
+        links: [
+          {
+            label: "Explore experimental learning",
+            icon: "chevron-right",
+            iconPosition: "right",
+            size: "small",
+          },
+          {
+            label: "Explore experimental learning",
+            icon: "chevron-right",
+            iconPosition: "right",
+            size: "small",
+          },
+          {
+            label: "Explore experimental learning",
+            icon: "chevron-right",
+            iconPosition: "right",
+            size: "small",
+          },
+        ],
+      },
+    },
+    {
+      name: "Large",
+      context: {
+        links: [
+          {
+            label: "Explore experimental learning",
+            icon: "chevron-right",
+            iconPosition: "right",
+            size: "large",
+          },
+          {
+            label: "Explore experimental learning",
+            icon: "chevron-right",
+            iconPosition: "right",
+            size: "large",
+          },
+          {
+            label: "Explore experimental learning",
+            icon: "chevron-right",
+            iconPosition: "right",
+            size: "large",
+          },
+        ],
+      },
+    },
+    {
+      name: "Left Icon",
+      context: {
+        links: [
+          {
+            label: "Explore experimental learning",
+            icon: "chevron-left",
+            iconPosition: "left",
+            size: "small",
+          },
+          {
+            label: "Explore experimental learning",
+            icon: "chevron-left",
+            iconPosition: "left",
+            size: "small",
+          },
+          {
+            label: "Explore experimental learning",
+            icon: "chevron-left",
+            iconPosition: "left",
+            size: "small",
+          },
+        ],
+      },
+    },
+  ],
 };
