925f097e6678e71e5c3b117f82b028b6958254dd
Byron Dominguez <byron.dominguez@verndale.com>
2021-07-30 10:19:46 -0500

    feat: Link list component

---
+++ src/components/link-list/link-list.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "Link list component",
+  status: "ready",
+  context: {
+    links: [
+      {
+        label: "Explore experimental learning",
+        icon: "arrow-right",
+      },
+      {
+        label: "Explore experimental learning",
+        icon: "arrow-right",
+      },
+      {
+        label: "Explore experimental learning",
+        icon: "arrow-right",
+      },
+    ],
+  },
+};
