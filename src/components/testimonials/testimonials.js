45bc953f2ee5927b3e9c1fe1055b562bf4ee991a
Esteban <esteban_fs@hotmail.com>
2021-08-25 17:13:38 -0500

    Remove console log

--- src/components/testimonials/testimonials.js
+++ src/components/testimonials/testimonials.js
@@ -11,11 +11,8 @@ class Module extends Component {
       next: this.el.querySelector('.swiper-button-next'),
       prev: this.el.querySelector('.swiper-button-prev')
     }
-    console.log(this.dom)
     this.initSlider()
   }
-
-  addListeners() {}
 
   initSlider() {
     this.slider = new Swiper(this.dom.container, {
