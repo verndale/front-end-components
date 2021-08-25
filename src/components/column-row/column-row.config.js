202b1e2fedf0485f53e5542995d4a51c7c2bddba
Esteban <esteban_fs@hotmail.com>
2021-08-24 20:18:40 -0500

    Column markup, config and styles

---
+++ src/components/column-row/column-row.config.js
@@ -0,0 +1,112 @@
+module.exports = {
+  title: 'columns row',
+  status: 'wip',
+  // eslint-disable-next-line quote-props
+  default: '4_columns_row',
+  context: {
+    heading: '4 columns row',
+    copy: 'Praesent dui elit, porttitor sed vulputate id, mollis sed nibh. Morbi molestie scelerisque diam, at efficitur erat auctor vitae. Proin vehicula volutpat consequat. Donec sit amet magna sed urna egestas feugiat in et nisi. Suspendisse risus dolor, maximus sit amet pharetra sed, convallis sed orci. Nulla sit amet nisi cursus, blandit ipsum vel, feugiat nisl.',
+    items: [
+      {
+        title: 'Item Title',
+        heading: 'Item Heading',
+        content:
+          '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>'
+      },
+      {
+        title: 'Item Title',
+        heading: 'Item Heading',
+        content:
+          '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><img src="https://verndale-image-tools.herokuapp.com/id/EFvP9cHipMQ?w=900&h=450" alt="Image Description">'
+      },
+      {
+        title: 'Item Title',
+        heading: 'Item Heading',
+        content:
+          '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><video controls="controls"><source src="/images/video.mp4" type="video/mp4"></video>'
+      },
+      {
+        title: 'Item Title',
+        heading: 'Item Heading',
+        content:
+          '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><div class="buttons-row"><a href="#" class="btn btn--primary"><svg viewBox="0 0 30 30"><use xlink:href="#file-pdf" /></svg>Document Name</a><a href="#" class="btn btn--primary"><svg viewBox="0 0 30 30"><use xlink:href="#file-pdf" /></svg>Document Name</a></div>'
+      }
+    ]
+  },
+  variants: [
+    {
+      name: '4_columns_row',
+      context: {
+        heading: '4 columns row',
+        type: 'four',
+        copy: 'Praesent dui elit, porttitor sed vulputate id, mollis sed nibh. Morbi molestie scelerisque diam, at efficitur erat auctor vitae. Proin vehicula volutpat consequat. Donec sit amet magna sed urna egestas feugiat in et nisi. Suspendisse risus dolor, maximus sit amet pharetra sed, convallis sed orci. Nulla sit amet nisi cursus, blandit ipsum vel, feugiat nisl.',
+        items: [
+          {
+            title: 'Item Title',
+            heading: 'Item Heading',
+            content:
+              '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>'
+          },
+          {
+            title: 'Item Title',
+            heading: 'Item Heading',
+            content:
+              '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><img src="https://verndale-image-tools.herokuapp.com/id/EFvP9cHipMQ?w=900&h=450" alt="Image Description">'
+          },
+          {
+            title: 'Item Title',
+            heading: 'Item Heading',
+            content:
+              '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><video controls="controls"><source src="/images/video.mp4" type="video/mp4"></video>'
+          },
+          {
+            title: 'Item Title',
+            heading: 'Item Heading',
+            content:
+              '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><div class="buttons-row"><a href="#" class="btn btn--primary"><svg viewBox="0 0 30 30"><use xlink:href="#file-pdf" /></svg>Document Name</a><a href="#" class="btn btn--primary"><svg viewBox="0 0 30 30"><use xlink:href="#file-pdf" /></svg>Document Name</a></div>'
+          },
+          {
+            title: 'Item Title',
+            heading: 'Item Heading',
+            content:
+              '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><div class="buttons-row"><a href="#" class="btn btn--primary"><svg viewBox="0 0 30 30"><use xlink:href="#file-pdf" /></svg>Document Name</a><a href="#" class="btn btn--primary"><svg viewBox="0 0 30 30"><use xlink:href="#file-pdf" /></svg>Document Name</a></div>'
+          }
+        ]
+      }
+    },
+    {
+      name: '3_columns_row',
+      context: {
+        heading: '3 columns row',
+        type: 'three',
+        copy: 'Praesent dui elit, porttitor sed vulputate id, mollis sed nibh. Morbi molestie scelerisque diam, at efficitur erat auctor vitae. Proin vehicula volutpat consequat. Donec sit amet magna sed urna egestas feugiat in et nisi. Suspendisse risus dolor, maximus sit amet pharetra sed, convallis sed orci. Nulla sit amet nisi cursus, blandit ipsum vel, feugiat nisl.',
+        items: [
+          {
+            title: 'Item Title',
+            heading: 'Item Heading',
+            content:
+              '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>'
+          },
+          {
+            title: 'Item Title',
+            heading: 'Item Heading',
+            content:
+              '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><img src="https://verndale-image-tools.herokuapp.com/id/EFvP9cHipMQ?w=900&h=450" alt="Image Description">'
+          },
+          {
+            title: 'Item Title',
+            heading: 'Item Heading',
+            content:
+              '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><video controls="controls"><source src="/images/video.mp4" type="video/mp4"></video>'
+          },
+          {
+            title: 'Item Title',
+            heading: 'Item Heading',
+            content:
+              '<p>Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p><div class="buttons-row"><a href="#" class="btn btn--primary"><svg viewBox="0 0 30 30"><use xlink:href="#file-pdf" /></svg>Document Name</a><a href="#" class="btn btn--primary"><svg viewBox="0 0 30 30"><use xlink:href="#file-pdf" /></svg>Document Name</a></div>'
+          }
+        ]
+      }
+    }
+  ]
+}
