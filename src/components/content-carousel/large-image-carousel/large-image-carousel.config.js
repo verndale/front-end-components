62c61631d39414d5aada6d706b71b49b0421b34f
Marcelo Bonilla <marcelo.bonilla@QTO-DPC-01823.local>
2021-09-15 16:41:07 -0500

    ST-2397 C-CLS-1 carousel container

--- src/components/content-carousel/large-image-carousel/large-image-carousel.config.js
+++ src/components/content-carousel/large-image-carousel/large-image-carousel.config.js
@@ -2,10 +2,9 @@ module.exports = {
     title: 'Large Image Carousel',
     status: 'ready',
     context: {
-        background: "Lorem ipsum dolor sit amet",
+        definition: "Lorem ipsum dolor sit amet",
         title: "Lorem ipsum dolor sit amet",
-        slides: [
-            {
+        slides: [{
                 image: "https://via.placeholder.com/883x890",
                 imageDescription: "Image descriptionLorem ipsum dolor sit amet",
                 title: "Lorem ipsum dolor sit amet",
@@ -33,8 +32,91 @@ module.exports = {
                 link: null
             }
         ],
-        previousSlideLabel: "Previous Slide", 
+        previousSlideLabel: "Previous Slide",
         nextSlideLabel: "Next Slide"
     },
-    variants: []
+    variants: [{
+            name: 'with Subheader',
+            context: {
+                definition: "Lorem ipsum dolor sit amet",
+                title: "Lorem ipsum dolor sit amet",
+                slides: [{
+                        image: 'https://via.placeholder.com/282x159',
+                        heading: 'Default',
+                        copy: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.',
+                        ctaLabel: 'Action',
+                        cta: '#',
+                        icon: 'chevron-right'
+                    },
+                    {
+                        image: 'https://via.placeholder.com/282x159',
+                        heading: 'Default',
+                        copy: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.',
+                        ctaLabel: 'Action',
+                        cta: '#',
+                        icon: 'chevron-right'
+                    },
+                    {
+                        image: 'https://via.placeholder.com/282x159',
+                        heading: 'Default',
+                        copy: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.',
+                        ctaLabel: 'Action',
+                        cta: '#',
+                        icon: 'chevron-right'
+                    },
+                    {
+                        image: 'https://via.placeholder.com/282x159',
+                        heading: 'Default',
+                        copy: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.',
+                        ctaLabel: 'Action',
+                        cta: '#',
+                        icon: 'chevron-right'
+                    }
+                ],
+                previousSlideLabel: "Previous Slide",
+                nextSlideLabel: "Next Slide"
+            }
+        },
+        {
+            name: 'without Subheader',
+            context: {
+                title: "Lorem ipsum dolor sit amet",
+                slides: [{
+                        image: 'https://via.placeholder.com/282x159',
+                        heading: 'Default',
+                        copy: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.',
+                        ctaLabel: 'Action',
+                        cta: '#',
+                        icon: 'chevron-right'
+                    },
+                    {
+                        image: 'https://via.placeholder.com/282x159',
+                        heading: 'Default',
+                        copy: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.',
+                        ctaLabel: 'Action',
+                        cta: '#',
+                        icon: 'chevron-right'
+                    },
+                    {
+                        image: 'https://via.placeholder.com/282x159',
+                        heading: 'Default',
+                        copy: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.',
+                        ctaLabel: 'Action',
+                        cta: '#',
+                        icon: 'chevron-right'
+                    },
+                    {
+                        image: 'https://via.placeholder.com/282x159',
+                        heading: 'Default',
+                        copy: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.',
+                        ctaLabel: 'Action',
+                        cta: '#',
+                        icon: 'chevron-right'
+                    }
+                ],
+                previousSlideLabel: "Previous Slide",
+                nextSlideLabel: "Next Slide"
+            }
+        },
+    ]
 }
