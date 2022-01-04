8efecc0785a023f82944f63ce43c78ab76025470
Byron Dominguez <byron.dominguez@verndale.com>
2022-01-04 17:44:45 -0500

    fix: ST-2555 - Fix styles and sass variables

--- src/components/C-CRD-8-carousel-content-card/C-CRD-8-carousel-content-card.config.js
+++ src/components/C-CRD-8-carousel-content-card/C-CRD-8-carousel-content-card.config.js
@@ -2,31 +2,52 @@ module.exports = {
   title: 'C-CRD-8: Carousel Content Card',
   status: 'ready',
   context: {
-    image: 'https://via.placeholder.com/282x159',
+    image: {
+      srcset: {
+        767: 'https://verndale-image-tools.herokuapp.com/id/9wg5jCEPBsw?w=768&h=414',
+        1279: 'https://verndale-image-tools.herokuapp.com/id/9wg5jCEPBsw?w=1280&h=690'
+      },
+      src: 'https://verndale-image-tools.herokuapp.com/id/9wg5jCEPBsw?w=1600&h=700',
+      description: 'Image Description'
+    },
     heading: 'Default',
     copy: '<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.</p>',
     ctaLabel: 'Action',
-    cta: '#',
+    cta: '#'
   },
   variants: [
     {
       name: 'Card with Copy',
       context: {
-        image: 'https://via.placeholder.com/282x159',
+        image: {
+          srcset: {
+            767: 'https://verndale-image-tools.herokuapp.com/id/9wg5jCEPBsw?w=768&h=414',
+            1279: 'https://verndale-image-tools.herokuapp.com/id/9wg5jCEPBsw?w=1280&h=690'
+          },
+          src: 'https://verndale-image-tools.herokuapp.com/id/9wg5jCEPBsw?w=1600&h=700',
+          description: 'Image Description'
+        },
         heading: 'Default',
         copy: '<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.</p>',
         ctaLabel: 'Action',
-        cta: '#',
+        cta: '#'
       }
     },
     {
       name: 'Card without Copy',
       context: {
-        image: 'https://via.placeholder.com/282x159',
+        image: {
+          srcset: {
+            767: 'https://verndale-image-tools.herokuapp.com/id/9wg5jCEPBsw?w=768&h=414',
+            1279: 'https://verndale-image-tools.herokuapp.com/id/9wg5jCEPBsw?w=1280&h=690'
+          },
+          src: 'https://verndale-image-tools.herokuapp.com/id/9wg5jCEPBsw?w=1600&h=700',
+          description: 'Image Description'
+        },
         heading: 'Default',
         copy: null,
         ctaLabel: 'Action',
-        cta: '#',
+        cta: '#'
       }
     }
   ]
